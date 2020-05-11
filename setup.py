b63e99bef7f762f43042d7246c54f91d005422c5
Georgios Chochlakis <georgioschochlakis@gmail.com>
2020-05-11 18:57:58 +0300

    Update setup

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='crnns4captions',
-    version='0.1.0',
+    version='v0.2',
     packages=find_packages(),
     scripts=[],
-    url='https://github.com/gchochla/CRNNs-for-Visual-Descriptions',
+    url='https://github.com/gchochla/Deep-Representations-of-Visual-Descriptions',
     author='Georgios Chochlakis',
     install_requires=[],
     extras_require={
